aa0d27e44a18ad22ce45b17fe09a7d2dd884963c
mconstantino123 <60445977+mconstantino123@users.noreply.github.com>
2020-08-18 15:57:23 -0400

    Update geolocation.py

--- geolocation.py
+++ geolocation.py
@@ -1,7 +1,5 @@
 import csv
 import requests
-import xlwt
-from xlwt import Workbook
 import time
 from time import sleep
 import pandas as pd
@@ -9,29 +7,34 @@ import pandas as pd
 def main():
     locate()
 
+def exportCSV(locs):
+    df = pd.DataFrame(locs)
+    df.to_csv('results.csv')
+    
 def locate():
     locs = []
     ips = readCSVfile()
     count = 0
     total = 0
     for i in ips:
-        #if count == 5:
+        #if count == 5: 
             #count = 0
             #time.sleep(5)
+        #sometimes the API doesnt like the amount of requests you send it and you might
+        #need to make your program sleep in order to be able to return the full list
         browser = requests.session()
         result = browser.get(f"https://freegeoip.app/json/{i}").json()
-        count += 1
-        total += 1
-        print(total)
+        #count += 1
+        #total += 1
+        #print(total)
         locs.append(result)
-    df = pd.DataFrame(locs)
-    df.to_csv('results.csv')
+    exportCSV(locs)
 def readCSVfile():
     l = []
     with open('input2.csv') as f:
         csvReader = csv.reader(f, delimiter = ',')
         for row in csvReader:
-            l.append(row[3])
+            l.append(row[3]) #change this to whatever row your ips are in
     return l
 
 if __name__ == '__main__':
